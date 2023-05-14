bebba97e9cdc2be75d6ef3dfb7f8e338afc270b9
warmland_edger <luren145@gmail.com>
2023-05-14 17:00:53 +0800

    x

--- src/CPU/dm.v
+++ src/CPU/dm.v
@@ -1,7 +1,7 @@
 module DM(
     input clk,
     input mem_write,
-    input [31:0] addr, //addr[7:2]读写地址
+    input [31:0] addr, //按字节访问,必须要4字节对齐，否则结果未定义
     input [31:0] data_in, //写入数据
     output [31:0] data_out //读出数据
 );
